8e1eb052a03a193e8db0c2c3ddeaf5503ba9b847
AgungAryansyah <agungaryansyah2006@gmail.com>
2026-05-31 07:46:56 +0700

    feat: export LSTMModel from models package

--- src/fhad_daic/models/__init__.py
+++ src/fhad_daic/models/__init__.py
@@ -2,5 +2,6 @@ from .tcn import CausalConv1d, TCN, TCNBlock
 from .mil import MILTCN
 from .mlp import MLP
 from .gru import GRUModel
+from .lstm import LSTMModel
 
-__all__ = ["CausalConv1d", "TCNBlock", "TCN", "MILTCN", "MLP", "GRUModel"]
+__all__ = ["CausalConv1d", "TCNBlock", "TCN", "MILTCN", "MLP", "GRUModel", "LSTMModel"]
